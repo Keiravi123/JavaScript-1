ea912dd4a97650d7b12e8ddd7a4aba0635db6460
Keiravi123 <150300716+Keiravi123@users.noreply.github.com>
2024-03-20 16:58:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const birthYear = 2007
 console.log("My age is ",age)
 let sum = age + 6
 console.log("In 6 years I will be ",sum)
-if (age <= 18 && age <=30) {
+if (age <= 18 & age <=30) {
   console.log("You are in your twenties or late teens ")
 }
 let isMirror = age <18;
